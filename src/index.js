6dff8ea16af25dff6d6f3da0a0ffb1088ac56d65
yakshina <yakshina_kristina@mail.ru>
2021-03-06 17:53:03 +0300

    fix: change function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    
+    if (n < 0) {
+        n = (-1)*n; 
+    }
+    
+    let str = n + "";
+    str = str.split('').reverse().join('');
+
+    return str;
 }
